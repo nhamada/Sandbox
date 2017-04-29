3e6512adb2e2bdbe24940afd400c670cbf740ecc
Naohiro Hamada <nao.pinguinus@icloud.com>
2017-04-29 20:34:44 +0900

    Modify zoom behavior
    Initially, image is centered with fitted size.

--- ScrollableImageView/ScrollableImageView/ScrollableImageView.swift
+++ ScrollableImageView/ScrollableImageView/ScrollableImageView.swift
@@ -6,6 +6,13 @@
 //  Copyright © 2017年 Naohiro Hamada. All rights reserved.
 //
 
+/*
+ * 参考
+ * https://www.raywenderlich.com/122139/uiscrollview-tutorial
+ * http://qiita.com/shift_option_k/items/eea52a662ae64258fb6f
+ */
+
+
 import UIKit
 
 final public class ScrollableImageView: UIScrollView {
@@ -23,6 +30,8 @@ final public class ScrollableImageView: UIScrollView {
         }
     }
     
+    private var imageViewConstraint: (top: NSLayoutConstraint, bottom: NSLayoutConstraint, left: NSLayoutConstraint, right: NSLayoutConstraint)! = nil
+    
     override public init(frame: CGRect) {
         super.init(frame: frame)
         
@@ -36,13 +45,22 @@ final public class ScrollableImageView: UIScrollView {
     }
     
     private func didLoad() {
+        translatesAutoresizingMaskIntoConstraints = false
+        contentMode = .center
+        
         imageView.contentMode = .scaleAspectFit
         imageView.translatesAutoresizingMaskIntoConstraints = false
         addSubview(imageView)
-        NSLayoutConstraint.activate([imageView.topAnchor.constraint(equalTo: topAnchor),
-                                     imageView.bottomAnchor.constraint(equalTo: bottomAnchor),
-                                     imageView.leftAnchor.constraint(equalTo: leftAnchor),
-                                     imageView.rightAnchor.constraint(equalTo: rightAnchor)])
+        imageViewConstraint = (imageView.topAnchor.constraint(equalTo: topAnchor),
+                               imageView.bottomAnchor.constraint(equalTo: bottomAnchor),
+                               imageView.leftAnchor.constraint(equalTo: leftAnchor),
+                               imageView.rightAnchor.constraint(equalTo: rightAnchor))
+        NSLayoutConstraint.activate([imageViewConstraint.top,
+                                     imageViewConstraint.bottom,
+                                     imageViewConstraint.left,
+                                     imageViewConstraint.right,])
+        
+        bouncesZoom = true
         
         delegate = self
     }
@@ -51,22 +69,46 @@ final public class ScrollableImageView: UIScrollView {
         guard let image = imageView.image else {
             fatalError("`updateScale` must be called after image is given.")
         }
-        if image.size.width < image.size.height {
-            updateScale(bounds.height / image.size.height)
-        } else {
-            updateScale(bounds.width / image.size.width)
-        }
         
-        zoom(to: CGRect(x: 0, y: 0, width: image.size.width, height: image.size.height), animated: false)
+        let widthScale = bounds.width / image.size.width
+        let heightScale = bounds.height / image.size.height
+        let minScale = min(widthScale, heightScale)
+        minimumZoomScale = minScale
+        maximumZoomScale = minScale * 4
+        zoomScale = minScale
     }
     
-    private func updateScale(_ baseScale: CGFloat) {
-        minimumZoomScale = baseScale
-        maximumZoomScale = baseScale * 4.0
+    fileprivate func updateConstraints(with size: CGSize) {
+        let offset: (x: CGFloat, y: CGFloat) = (frameOriginOffsetForX(size.width),
+                                                frameOriginOffsetForY(size.height))
+        imageViewConstraint.left.constant = offset.x
+        imageViewConstraint.right.constant = offset.x
+        imageViewConstraint.top.constant = offset.y
+        imageViewConstraint.bottom.constant = offset.y
+        
+        layoutIfNeeded()
+    }
+    
+    private func frameOriginOffsetForX(_ targetWidth: CGFloat) -> CGFloat {
+        guard let image = imageView.image else {
+            fatalError("Image is nil.")
+        }
+        return max(0, (targetWidth - image.size.width * zoomScale) / 2)
+    }
+    
+    private func frameOriginOffsetForY(_ targetHeight: CGFloat) -> CGFloat {
+        guard let image = imageView.image else {
+            fatalError("Image is nil.")
+        }
+        return max(0, (targetHeight - image.size.height * zoomScale) / 2)
     }
 }
 
 extension ScrollableImageView: UIScrollViewDelegate {
+    public func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        updateConstraints(with: scrollView.bounds.size)
+    }
+    
     public func viewForZooming(in scrollView: UIScrollView) -> UIView? {
         return imageView
     }
