16d41bf7fb241b92878541db7c44883961dcce4e
Naohiro Hamada <nao.pinguinus@icloud.com>
2017-05-25 01:35:10 +0900

    Add observers for keyboard
    Add & remove observer via NotificationCenter

--- KeyboardView/KeyboardView/ViewController.swift
+++ KeyboardView/KeyboardView/ViewController.swift
@@ -20,6 +20,72 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(onKeyboardWillShow(notification:)),
+                                       name: Notification.Name.UIKeyboardWillShow, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(onKeyboardDidShow(notification:)),
+                                       name: Notification.Name.UIKeyboardDidShow, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(onKeyboardWillHide(notification:)),
+                                       name: Notification.Name.UIKeyboardWillHide, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(onKeyboardDidHide(notification:)),
+                                       name: Notification.Name.UIKeyboardDidHide, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(onKeyboardWillChangeFrame(notification:)),
+                                       name: Notification.Name.UIKeyboardWillChangeFrame, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(onKeyboardDidChangeFrame(notification:)),
+                                       name: Notification.Name.UIKeyboardDidChangeFrame, object: nil)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.removeObserver(self, name: Notification.Name.UIKeyboardWillShow, object: nil)
+        notificationCenter.removeObserver(self, name: Notification.Name.UIKeyboardDidShow, object: nil)
+        notificationCenter.removeObserver(self, name: Notification.Name.UIKeyboardWillHide, object: nil)
+        notificationCenter.removeObserver(self, name: Notification.Name.UIKeyboardDidHide, object: nil)
+        notificationCenter.removeObserver(self, name: Notification.Name.UIKeyboardWillChangeFrame, object: nil)
+        notificationCenter.removeObserver(self, name: Notification.Name.UIKeyboardDidChangeFrame, object: nil)
+    }
 
+    // MARK:- Keyboard notifications
+    @objc private func onKeyboardWillShow(notification: Notification) {
+        log(notification: notification)
+    }
+    
+    @objc private func onKeyboardDidShow(notification: Notification) {
+        log(notification: notification)
+    }
+    
+    @objc private func onKeyboardWillHide(notification: Notification) {
+        log(notification: notification)
+    }
+    
+    @objc private func onKeyboardDidHide(notification: Notification) {
+        log(notification: notification)
+    }
+    
+    @objc private func onKeyboardWillChangeFrame(notification: Notification) {
+        log(notification: notification)
+    }
+    
+    @objc private func onKeyboardDidChangeFrame(notification: Notification) {
+        log(notification: notification)
+    }
+    
+    // MARK:- Private methods
+    private func log(notification: Notification, function: String = #function) {
+        print("----------")
+        print("\(function)")
+        print("\tnotification.name = \(notification.name)")
+        guard let userInfo = notification.userInfo else {
+            return
+        }
+        print("\tnotification.userInfo =")
+        for (k, v) in userInfo {
+            print("\t\t\(k)=\(v)")
+        }
+    }
 }
-
